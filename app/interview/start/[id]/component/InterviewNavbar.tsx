ed0e0295e30a1776f2bd0e5926ba4fec0aca01af
AskarMusthaffa <askar.musthaffa@bcstechnology.com.au>
2025-02-06 00:50:38 +0530

    ...

--- app/interview/start/[id]/component/InterviewNavbar.tsx
+++ app/interview/start/[id]/component/InterviewNavbar.tsx
@@ -14,36 +14,15 @@ interface InterviewNavbarProps {
   onInterviewComplete: () => void;
 }
 
-const InterviewNavbar: React.FC<InterviewNavbarProps> = ({
-  company,
-  totalMinutes,
-  startTime,
-  onInterviewComplete,
-}) => {
+const InterviewNavbar: React.FC<InterviewNavbarProps> = ({ company, totalMinutes, startTime, onInterviewComplete }) => {
   return (
-    <nav className="flex items-center justify-between w-full bg-white shadow-md  ">
+    <nav className='flex items-center justify-between w-full bg-white'>
       {/* Company Logo or Name */}
-      <div className="flex items-center">
-        {company.logo ? (
-          <Image
-            src={company.logo}
-            alt={`${company.name} Logo`}
-            width={100}
-            height={40}
-            className="w-auto min-h-[30px] max-h-[40px] object-contain"
-          />
-        ) : (
-          <p className="font-bold text-lg text-gray-800">{company.name}</p>
-        )}
-      </div>
+      <div className='flex items-center'>{company.logo ? <Image src={company.logo} alt={`${company.name} Logo`} width={100} height={40} className='w-auto min-h-[30px] max-h-[40px] object-contain' /> : <p className='font-bold text-lg text-gray-800'>{company.name}</p>}</div>
 
       {/* Countdown Timer */}
-      <div className="flex items-center gap-2">
-        <CountdownTimer
-          totalMinutes={totalMinutes}
-          startTime={startTime}
-          onComplete={onInterviewComplete}
-        />
+      <div className='flex items-center gap-2'>
+        <CountdownTimer totalMinutes={totalMinutes} startTime={startTime} onComplete={onInterviewComplete} />
       </div>
     </nav>
   );
