3b4fd085f29f30c49299a633110f477647f62af3
AskarMusthaffa <askar.musthaffa@bcstechnology.com.au>
2025-02-11 19:51:47 +0530

    .

--- services/dashboard.service.ts
+++ services/dashboard.service.ts
@@ -1,6 +1,6 @@
 import apiClient from "@/helpers/apiClient";
 
 export const get10InterviewResult = async () => {
-  const response = await apiClient.get("/dashboard/interview-results?limit=10");
+  const response = await apiClient.get("/dashboard/interview-results?limit=5");
   return response.data.data;
 };
