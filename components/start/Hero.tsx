c1dc04a41a341576682959447c23dfa4985c1304
AskarMusthaffa <askar.musthaffa@bcstechnology.com.au>
2025-02-17 17:40:53 +0530

    ....

--- components/start/Hero.tsx
+++ components/start/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Hero: React.FC = () => {
   return (
-    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32'>
+    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-25 text-center lg:pt-32'>
       <h1 className='mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl'>
         Hiring{" "}
         <span className='relative whitespace-nowrap text-blue-600'>
