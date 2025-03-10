61a1161dd6b3e63271e1d1079e96fab3e69d4ec2
AskarMusthaffa <askar.musthaffa@bcstechnology.com.au>
2025-03-10 11:44:21 +0530

    ...

--- components/auth/register.tsx
+++ components/auth/register.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import { toast } from "react-toastify";
+import { Logo } from "../logo";
 
 export const Register = () => {
   const router = useRouter();
@@ -42,72 +43,36 @@ export const Register = () => {
 
   return (
     <>
-      <div className="text-center text-[25px] font-bold mb-6">Register</div>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={RegisterSchema}
-        onSubmit={handleRegister}
-      >
-        {({ values, errors, touched, handleChange, handleSubmit }) => (
-          <>
-            <div className="flex flex-col w-1/2 gap-4 mb-4">
-              <Input
-                variant="bordered"
-                label="Name"
-                value={values.name}
-                isInvalid={!!errors.name && !!touched.name}
-                errorMessage={errors.name}
-                onChange={handleChange("name")}
-              />
-              <Input
-                variant="bordered"
-                label="Email"
-                type="email"
-                value={values.email}
-                isInvalid={!!errors.email && !!touched.email}
-                errorMessage={errors.email}
-                onChange={handleChange("email")}
-              />
-              <Input
-                variant="bordered"
-                label="Password"
-                type="password"
-                value={values.password}
-                isInvalid={!!errors.password && !!touched.password}
-                errorMessage={errors.password}
-                onChange={handleChange("password")}
-              />
-              <Input
-                variant="bordered"
-                label="Confirm password"
-                type="password"
-                value={values.confirmPassword}
-                isInvalid={
-                  !!errors.confirmPassword && !!touched.confirmPassword
-                }
-                errorMessage={errors.confirmPassword}
-                onChange={handleChange("confirmPassword")}
-              />
-            </div>
+      <main className='mx-auto w-full max-w-md sm:px-4 md:w-96 md:max-w-sm md:px-0'>
+        <div className='flex'>
+          <Logo />
+        </div>
+        <h2 className='mt-20 text-lg font-semibold text-gray-900'>Sign up for a new account.</h2>
 
-            <Button
-              onPress={() => handleSubmit()}
-              variant="flat"
-              color="primary"
-              isLoading={isSubmitting}
-            >
-              Register
-            </Button>
-          </>
-        )}
-      </Formik>
+        <div className='mt-2 text-sm  '>
+          Already have an account ?{" "}
+          <Link href='/login' className='font-medium text-blue-600 hover:underline'>
+            Login here
+          </Link>
+        </div>
 
-      <div className="font-light text-slate-400 mt-4 text-sm">
-        Already have an account ?{" "}
-        <Link href="/login" className="font-bold">
-          Login here
-        </Link>
-      </div>
+        <Formik initialValues={initialValues} validationSchema={RegisterSchema} onSubmit={handleRegister}>
+          {({ values, errors, touched, handleChange, handleSubmit }) => (
+            <>
+              <div className='mt-10 grid grid-cols-1 gap-y-8'>
+                <Input variant='bordered' label='Name' value={values.name} isInvalid={!!errors.name && !!touched.name} errorMessage={errors.name} onChange={handleChange("name")} />
+                <Input variant='bordered' label='Email' type='email' value={values.email} isInvalid={!!errors.email && !!touched.email} errorMessage={errors.email} onChange={handleChange("email")} />
+                <Input variant='bordered' label='Password' type='password' value={values.password} isInvalid={!!errors.password && !!touched.password} errorMessage={errors.password} onChange={handleChange("password")} />
+                <Input variant='bordered' label='Confirm password' type='password' value={values.confirmPassword} isInvalid={!!errors.confirmPassword && !!touched.confirmPassword} errorMessage={errors.confirmPassword} onChange={handleChange("confirmPassword")} />
+
+                <Button onPress={() => handleSubmit()} radius='full' color='primary' isLoading={isSubmitting}>
+                  Register
+                </Button>
+              </div>
+            </>
+          )}
+        </Formik>
+      </main>
     </>
   );
 };
