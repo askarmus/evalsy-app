3870e6f28b5d9f26173f74329dedfd68afcfc34e
AskarMusthaffa <askar.musthaffa@bcstechnology.com.au>
2025-04-16 06:47:21 +0530

    refactor: remove unused CORS headers and delete vercel configuration file

--- next.config.js
+++ next.config.js
@@ -5,23 +5,6 @@ const nextConfig = {
   images: {
     domains: ["4acbt7lbir1rsx1d.public.blob.vercel-storage.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://www.evalsy.com",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
