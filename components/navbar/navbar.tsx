436808aa6faba01414eb6c4bff00e1e0ed32f241
askarmus <askarmus@hotmail.com>
2025-05-13 00:44:00 +0530

    style: standardize quotation marks and improve code formatting in Navbar component

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,12 +1,10 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@heroui/react";
-import React from "react";
-import { BurguerButton } from "./burguer-button";
-import { UserDropdown } from "./user-dropdown";
-import Link from "next/link";
-import { DarkModeSwitch } from "./darkmodeswitch";
-import { AiOutlineHome, AiOutlineNotification, AiOutlineSetting, AiOutlineShopping } from "react-icons/ai";
-import { usePathname } from "next/navigation";
-import { DarkLogo } from "../shared/dark-logo";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from '@heroui/react';
+import React from 'react';
+import { UserDropdown } from './user-dropdown';
+import Link from 'next/link';
+import { AiOutlineHome, AiOutlineNotification, AiOutlineSetting, AiOutlineShopping } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import { DarkLogo } from '../shared/dark-logo';
 
 export const NavbarWrapper = () => {
   const currentPath = usePathname();
@@ -14,77 +12,78 @@ export const NavbarWrapper = () => {
 
   return (
     <Navbar
-      position='sticky'
+      position="sticky"
       isBordered
       onMenuOpenChange={setIsMenuOpen}
-      className='w-full bg-[#1E293B] text-[#F1F5F9]'
+      className="w-full bg-[#1E293B] text-[#F1F5F9]"
       classNames={{
-        wrapper: "w-full max-w-full",
-      }}>
-      <NavbarContent className='pr-3' justify='center'>
+        wrapper: 'w-full max-w-full',
+      }}
+    >
+      <NavbarContent className="pr-3" justify="center">
         <NavbarBrand>
-          <Link className='flex items-center hover:text-blue-200 gap-2' href='/dashboard'>
+          <Link className="flex items-center hover:text-blue-200 gap-2" href="/dashboard">
             <DarkLogo />
           </Link>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
-        <NavbarItem isActive={currentPath === "/dashboard"}>
-          <Link className='flex items-center text-md hover:text-blue-100 gap-2' href='/dashboard'>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem isActive={currentPath === '/dashboard'}>
+          <Link className="flex items-center text-md hover:text-blue-100 gap-2" href="/dashboard">
             <AiOutlineHome />
             Dashboard
           </Link>
         </NavbarItem>
 
-        <NavbarItem isActive={currentPath === "/interviews/list"}>
-          <Link className='flex items-center text-md hover:text-blue-100 gap-2' href='/interviews/list'>
+        <NavbarItem isActive={currentPath === '/interviews/list'}>
+          <Link className="flex items-center text-md hover:text-blue-100 gap-2" href="/interviews/list">
             <AiOutlineShopping />
             Interview
           </Link>
         </NavbarItem>
 
-        <NavbarItem isActive={currentPath === "/result"}>
-          <Link className='flex items-center text-md hover:text-blue-100 gap-2' href='/result'>
+        <NavbarItem isActive={currentPath === '/result'}>
+          <Link className="flex items-center text-md hover:text-blue-100 gap-2" href="/result">
             <AiOutlineNotification />
             Result
           </Link>
         </NavbarItem>
 
-        <NavbarItem isActive={currentPath === "/company/settings"}>
-          <Link className='flex items-center text-md hover:text-blue-100 gap-2' href='/company/settings'>
+        <NavbarItem isActive={currentPath === '/company/settings'}>
+          <Link className="flex items-center text-md hover:text-blue-100 gap-2" href="/company/settings">
             <AiOutlineSetting />
             Settings
           </Link>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify='end' className='w-fit data-[justify=end]:flex-grow-0'>
+      <NavbarContent justify="end" className="w-fit data-[justify=end]:flex-grow-0">
         <NavbarContent>
           <UserDropdown />
         </NavbarContent>
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className='sm:hidden' />
+        <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className="sm:hidden" />
       </NavbarContent>
 
       <NavbarMenu>
         <NavbarMenuItem>.</NavbarMenuItem>
         <NavbarMenuItem>
-          <Link href='/dashboard' className='w-full'>
+          <Link href="/dashboard" className="w-full">
             Dashboard
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link href='/interviews/list' className='w-full'>
+          <Link href="/interviews/list" className="w-full">
             Interview
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link href='/result' className='w-full'>
+          <Link href="/result" className="w-full">
             Result
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link href='/company/settings' className='w-full'>
+          <Link href="/company/settings" className="w-full">
             Settings
           </Link>
         </NavbarMenuItem>
