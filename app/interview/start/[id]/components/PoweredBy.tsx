58ff0cc90e9a2bf03d3cf393cbf0708c3be98005
askarmus <askarmus@hotmail.com>
2025-06-22 11:21:38 +0530

    feat: Add margin-top to PoweredBy component for improved layout spacing

--- app/interview/start/[id]/components/PoweredBy.tsx
+++ app/interview/start/[id]/components/PoweredBy.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PoweredBy: React.FC = () => {
   return (
-    <div className="flex justify-between items-center   text-sm text-slate-500">
+    <div className="flex justify-between items-center mt-2  text-sm text-slate-500">
       <div className="flex items-center gap-2"></div>
       <p className="text-xs">© 2025 Evalsy Interview Platform</p>
       <DarkModeSwitch />
