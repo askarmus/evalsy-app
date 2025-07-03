de548dfd7b0c277ae40c8308baf39ae6f72d4d5d
askarmus <askarmus@hotmail.com>
2025-07-03 13:18:07 +0530

    feat: Add Tidio chat script to LayoutWrapper for enhanced user interaction

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -14,6 +14,7 @@ export function LayoutWrapper({ children }: { children: React.ReactNode }) {
     <body className={clsx('font-sans antialiased scroll-smooth min-h-screen h-full', fontSans.className, isHome ? 'bg-[#fbfbfb]' : 'bg-gray-100 dark:bg-black')}>
       <GoogleAnalytic />
       <Providers>{children}</Providers>
+      <script src="//code.tidio.co/vpfusp81s6dcbg2k75locpwid8f1pmjl.js" async></script>
     </body>
   );
 }
