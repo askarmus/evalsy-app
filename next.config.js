c8a938880e738f0fbef92ef3af90978237c2e01f
askarmus <askarmus@hotmail.com>
2025-07-07 10:46:52 +0530

    feat: Add redirect configuration to next.config.js for homepage navigation

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   images: {
     domains: ['4acbt7lbir1rsx1d.public.blob.vercel-storage.com', 'storage.googleapis.com', 'pexels.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
