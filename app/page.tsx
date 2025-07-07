8fe488625bf0dd8a6171b2f48472a923d63efb5d
askarmus <askarmus@hotmail.com>
2025-07-07 10:59:02 +0530

    feat: Add pricing, demo request, and testimonials components; implement typewriter effect and chat integration

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { Header } from '@/components/start/Header';
+import { Hero } from '@/components/start/Hero';
+import { Pricing } from '@/components/start/Pricing';
+import { Footer } from '@/components/start/Footer';
+import { Testimonials } from '@/components/start/testimonials';
+import RequestDemo from '@/components/start/RequestDemo';
+import { OptimizationComparison } from '@/components/start/optimize-section';
+import Feature from '@/components/start/Feature';
+import FAQ from '@/components/start/FAQ';
+import Companies from '../components/start/Company';
+import TidioChat from '../components/start/TidioChat';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col text-black">
+      <main className="flex-1">
+        <Header />
+        <TidioChat />
+
+        <div className="bg-[url('/02.svg')] bg-cover bg-center">
+          <Hero />
+          <Companies />
+          <OptimizationComparison />
+        </div>
+        <Feature />
+        <Pricing />
+        <Testimonials />
+        <FAQ />
+        <RequestDemo />
+      </main>
+      <Footer />
+    </div>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
   images: {
     domains: ['4acbt7lbir1rsx1d.public.blob.vercel-storage.com', 'storage.googleapis.com', 'pexels.com'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
