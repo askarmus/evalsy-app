f3838aa71a57d63f6de239925bca50ed75e40696
askarmus <askarmus@hotmail.com>
2025-07-29 12:37:34 +0530

    .

--- app/interview/start/[id]/components/StartInterview.tsx
+++ app/interview/start/[id]/components/StartInterview.tsx
@@ -27,7 +27,7 @@ const StartInterview: React.FC = () => {
       {phase === 'not-started' && <InterviewInstruction />}
       {phase === 'in-progress' && <InterviewNavigator />}
       {(phase === 'completed' || phase === 'time-up') && <ThankYou />}
-      {phase === 'expired' && <NotFound />}
+      {phase === 'expired' && <ThankYou />}
     </div>
   );
 };
--- app/interview/start/[id]/components/ThankYou.tsx
+++ app/interview/start/[id]/components/ThankYou.tsx
@@ -1,21 +1,29 @@
 'use client';
-import React, { useEffect, useRef, useState } from 'react';
-import { useInterviewStore } from '../stores/useInterviewStore';
-import { Calendar, Card, CardBody, CardHeader } from '@heroui/react';
-import CandidateInfo from './CandidateInfo';
-import { motion } from 'framer-motion';
-import { FaCalendar, FaCheckCircle } from 'react-icons/fa';
+import React from 'react';
+import { Card, CardBody } from '@heroui/react';
+
 import PoweredBy from './PoweredBy';
-import { Mail } from 'lucide-react';
+import { Brain, CheckCircle, Sparkles } from 'lucide-react';
 
 const ThankYou: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900">
-      <div className="w-full max-w-screen-lg mx-auto px-6 py-8">
-        <Card className="p-8 bg-white dark:bg-gray-800" shadow="sm" radius="md">
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900 bg-[url('/02.svg')] bg-cover bg-center">
+      <div className="w-full max-w-screen-md mx-auto px-6 py-8">
+        <Card className="p-8 bg-white dark:bg-gray-800" shadow="lg" radius="md">
           <Card className="py-4 text-center bg-white dark:bg-gray-800" shadow="none">
             <CardBody className="overflow-visible py-2">
-              <div className="p-4 rounded-lg max-w-3xl mx-auto">
+              <div className="p-4   mx-auto">
+                <div className="flex justify-center mb-6">
+                  <div className={`rounded-full    "bg-green-100"}`}>
+                    <CheckCircle className={`w-16 h-16  "text-green-600"}`} />
+                  </div>
+                </div>
+                <div className="absolute top-0 right-0 w-32 h-32 opacity-10">
+                  <Sparkles className="w-full h-full text-purple-500" />
+                </div>
+                <div className="absolute bottom-0 left-0 w-24 h-24 opacity-10">
+                  <Brain className="w-full h-full text-purple-500" />
+                </div>
                 <div className="p-6 text-center">
                   <div className="space-y-4">
                     <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 dark:text-white mb-6">AI Interview Completed Successfully</h2>
