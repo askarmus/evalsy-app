8439719224d8068a9de5a258284ee8ae5a219c2b
askarmus <askarmus@hotmail.com>
2025-10-16 08:53:46 +0530

    .

--- app/(app)/company/settings/page.tsx
+++ app/(app)/company/settings/page.tsx
@@ -10,7 +10,7 @@ import { CreditTransactionTable } from '@/components/settings/components/credits
 import { ArrowLeftRight, Building, Building2, CreditCard, KeyRound, Lock } from 'lucide-react';
 
 const CompanySettingsPage = () => {
-  const [activeTab, setActiveTab] = useState('company');
+  const [activeTab, setActiveTab] = useState('settings');
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -21,8 +21,9 @@ const CompanySettingsPage = () => {
   }, []);
 
   const TABS = [
-    { key: 'company', label: 'Company', icon: Building },
+    { key: 'settings', label: 'Settings', icon: Building },
     { key: 'password', label: 'Password', icon: Lock },
+
     { key: 'subscriptions', label: 'Subscriptions', icon: CreditCard },
     { key: 'creditTransaction', label: 'Credit Transaction', icon: ArrowLeftRight },
   ];
@@ -32,7 +33,7 @@ const CompanySettingsPage = () => {
         <CardBody>
           <div className="grid lg:grid-cols-4 gap-3">
             <div className="col-span-1 p-4">
-              <nav className="flex flex-row lg:flex-col gap-2 w-auto lg:w-full bg-secondary-300 p-1.5 rounded-xl" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
+              <nav className="flex flex-row lg:flex-col gap-2 w-auto lg:w-full bg-secondary-500 p-1.5 rounded-xl" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
                 {TABS.map(({ key, label, icon: Icon }) => (
                   <button key={key} type="button" className={`flex items-center gap-2 text-start py-2 px-4 rounded-lg transition-colors duration-200 ${activeTab === key ? 'bg-white text-black' : 'text-white hover:bg-secondary-400'}`} onClick={() => setActiveTab(key)} aria-selected={activeTab === key} role="tab">
                     <Icon className="w-4 h-4" />
@@ -43,7 +44,7 @@ const CompanySettingsPage = () => {
             </div>
 
             <div className="lg:col-span-3 transition-all px-4 h-full">
-              {activeTab === 'company' && <CompanySettings />}
+              {activeTab === 'settings' && <CompanySettings />}
 
               {activeTab === 'password' && <ChangePassword />}
 
--- components/settings/company.tsx
+++ components/settings/company.tsx
@@ -8,11 +8,12 @@ import FileUploadWithPreview from '@/components/FileUploadWithPreview';
 import { CompanySettingsSchema } from '@/helpers/schemas';
 
 import { AiOutlineCloseCircle } from 'react-icons/ai';
-import { Building, Building2, Save, Upload } from 'lucide-react';
+import { Building, Building2, LayoutTemplate, Save, Upload } from 'lucide-react';
 
 export type CompanySettingsFormValues = {
   name: string;
   address?: string;
+  invitationTemplate?: string;
   about?: string;
   website?: string;
   linkedin?: string;
@@ -39,6 +40,9 @@ const CompanySettings = () => {
     phone: '',
     supportName: '',
     supportEmail: '',
+    invitationTemplate: `
+    You're invited to the Initial interviews for the <postion> role at <company>.
+    `,
   });
 
   const handleRemoveImage = () => {
@@ -72,72 +76,125 @@ const CompanySettings = () => {
     <Formik enableReinitialize initialValues={initialValues} validationSchema={CompanySettingsSchema} onSubmit={handleSubmit}>
       {({ values, errors, touched, setFieldValue, handleChange }) => (
         <Form>
-          <Card className="p-0" shadow="none">
-            <CardBody className="p-4">
-              <div className="mb-6 flex items-center gap-[5px] mb-3 md:mb-4 ">
-                <Building className="w-5 h-5 text-xl text-secondary-400" />
-                <h1 className=" text-xl/[24px] font-semibold text-tertiary  md:text-[20px]/[24px]">Company Setting</h1>
-              </div>
-              <div className="flex gap-4">
-                <div className="w-4/5">
-                  <div className="grid grid-cols-1 gap-4">
-                    <Input variant="bordered" size="sm" label="Company Name" name="name" value={values.name || ''} onChange={handleChange} isInvalid={!!errors.name && !!touched.name} errorMessage={errors.name} />
-                    <Textarea variant="bordered" size="sm" minRows={1} label="Address" name="address" value={values.address || ''} onChange={handleChange} isInvalid={!!errors.address && !!touched.address} errorMessage={errors.address} />
-                    <Textarea variant="bordered" size="sm" label="About Company" name="about" value={values.about || ''} onChange={handleChange} />
-
-                    <div className="grid grid-cols-2 gap-4">
-                      <Input variant="bordered" size="sm" label="Website" name="website" value={values.website || ''} onChange={handleChange} isInvalid={!!errors.website && !!touched.website} errorMessage={errors.website} />
-                      <Input variant="bordered" size="sm" label="LinkedIn" name="linkedin" value={values.linkedin || ''} onChange={handleChange} isInvalid={!!errors.linkedin && !!touched.linkedin} errorMessage={errors.linkedin} />
-                    </div>
-                    <div className="grid grid-cols-2 gap-4">
-                      <Input variant="bordered" size="sm" label="Facebook" name="facebook" value={values.facebook || ''} onChange={handleChange} isInvalid={!!errors.facebook && !!touched.facebook} errorMessage={errors.facebook} />
-                      <Input label="Twitter" name="twitter" value={values.twitter || ''} onChange={handleChange} isInvalid={!!errors.twitter && !!touched.twitter} errorMessage={errors.twitter} />
-                    </div>
-                    <Input variant="bordered" size="sm" label="Support Phone" name="phone" value={values.phone || ''} onChange={handleChange} isInvalid={!!errors.phone && !!touched.phone} errorMessage={errors.phone} />
-                    <Input variant="bordered" size="sm" label="Supporter Name" name="supportName" value={values.supportName || ''} onChange={handleChange} isInvalid={!!errors.supportName && !!touched.supportName} errorMessage={errors.supportName} />
-                    <Input variant="bordered" size="sm" label="Support Email" name="supportEmail" value={values.supportEmail || ''} onChange={handleChange} isInvalid={!!errors.supportEmail && !!touched.supportEmail} errorMessage={errors.supportEmail} />
+          <Tabs aria-label="Settings">
+            <Tab key="company" title="Company">
+              <Card className="p-0" shadow="none">
+                <CardBody className="p-4">
+                  <div className="mb-6 flex items-center gap-[5px] mb-3 md:mb-4 ">
+                    <Building className="w-5 h-5 text-xl text-secondary-400" />
+                    <h1 className=" text-xl/[24px] font-semibold text-tertiary  md:text-[20px]/[24px]">Company Setting</h1>
                   </div>
-                </div>
-                <div className="w-1/5">
-                  <div className="lg:col-span-1">
-                    <Card>
-                      <CardHeader>Company Logo</CardHeader>
-                      <CardBody>
-                        <div className="space-y-4">
-                          <div className="flex items-center justify-center w-full h-32 border-2 border-dashed border-secondary-200 rounded-lg bg-secondary-50">
-                            <div className="text-center">
-                              {uploadLogoUrl && (
-                                <div className="relative inline-block">
-                                  <img src={uploadLogoUrl} alt="Company Logo Preview" className="max-w-full h-auto rounded max-w-[80px]" />
-                                  <button type="button" onClick={handleRemoveImage} className="absolute -top-3 -right-3">
-                                    <AiOutlineCloseCircle />
-                                  </button>
-                                </div>
-                              )}
-                              <p className="text-sm text-gray-500">Current Logo</p>
-                            </div>
-                          </div>
+                  <div className="flex gap-4">
+                    <div className="w-4/5">
+                      <div className="grid grid-cols-1 gap-4">
+                        <Input variant="bordered" size="sm" label="Company Name" name="name" value={values.name || ''} onChange={handleChange} isInvalid={!!errors.name && !!touched.name} errorMessage={errors.name} />
+                        <Textarea variant="bordered" size="sm" minRows={1} label="Address" name="address" value={values.address || ''} onChange={handleChange} isInvalid={!!errors.address && !!touched.address} errorMessage={errors.address} />
+                        <Textarea variant="bordered" size="sm" label="About Company" name="about" value={values.about || ''} onChange={handleChange} />
 
-                          <FileUploadWithPreview
-                            onUpload={(data) => {
-                              setFieldValue('logo', data.url);
-                              setUploadLogoUrl(data.url);
-                            }}
-                          />
-                          <p className="text-xs text-gray-500 text-center">Supports: JPG, PNG, SVG (Max 2MB)</p>
+                        <div className="grid grid-cols-2 gap-4">
+                          <Input variant="bordered" size="sm" label="Website" name="website" value={values.website || ''} onChange={handleChange} isInvalid={!!errors.website && !!touched.website} errorMessage={errors.website} />
+                          <Input variant="bordered" size="sm" label="LinkedIn" name="linkedin" value={values.linkedin || ''} onChange={handleChange} isInvalid={!!errors.linkedin && !!touched.linkedin} errorMessage={errors.linkedin} />
                         </div>
-                      </CardBody>
-                    </Card>
+                        <div className="grid grid-cols-2 gap-4">
+                          <Input variant="bordered" size="sm" label="Facebook" name="facebook" value={values.facebook || ''} onChange={handleChange} isInvalid={!!errors.facebook && !!touched.facebook} errorMessage={errors.facebook} />
+                          <Input label="Twitter" name="twitter" value={values.twitter || ''} onChange={handleChange} isInvalid={!!errors.twitter && !!touched.twitter} errorMessage={errors.twitter} />
+                        </div>
+                        <Input variant="bordered" size="sm" label="Support Phone" name="phone" value={values.phone || ''} onChange={handleChange} isInvalid={!!errors.phone && !!touched.phone} errorMessage={errors.phone} />
+                        <Input variant="bordered" size="sm" label="Supporter Name" name="supportName" value={values.supportName || ''} onChange={handleChange} isInvalid={!!errors.supportName && !!touched.supportName} errorMessage={errors.supportName} />
+                        <Input variant="bordered" size="sm" label="Support Email" name="supportEmail" value={values.supportEmail || ''} onChange={handleChange} isInvalid={!!errors.supportEmail && !!touched.supportEmail} errorMessage={errors.supportEmail} />
+                      </div>
+                    </div>
+                    <div className="w-1/5">
+                      <div className="lg:col-span-1">
+                        <Card>
+                          <CardHeader>Company Logo</CardHeader>
+                          <CardBody>
+                            <div className="space-y-4">
+                              <div className="flex items-center justify-center w-full h-32 border-2 border-dashed border-secondary-200 rounded-lg bg-secondary-50">
+                                <div className="text-center">
+                                  {uploadLogoUrl && (
+                                    <div className="relative inline-block">
+                                      <img src={uploadLogoUrl} alt="Company Logo Preview" className="max-w-full h-auto rounded max-w-[80px]" />
+                                      <button type="button" onClick={handleRemoveImage} className="absolute -top-3 -right-3">
+                                        <AiOutlineCloseCircle />
+                                      </button>
+                                    </div>
+                                  )}
+                                  <p className="text-sm text-gray-500">Current Logo</p>
+                                </div>
+                              </div>
+
+                              <FileUploadWithPreview
+                                onUpload={(data) => {
+                                  setFieldValue('logo', data.url);
+                                  setUploadLogoUrl(data.url);
+                                }}
+                              />
+                              <p className="text-xs text-gray-500 text-center">Supports: JPG, PNG, SVG (Max 2MB)</p>
+                            </div>
+                          </CardBody>
+                        </Card>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </CardBody>
-            <CardFooter>
-              <Button type="submit" startContent={<Save />} isLoading={isLoading} color="secondary" variant="flat" radius="full">
-                Save Settings
-              </Button>
-            </CardFooter>
-          </Card>
+                </CardBody>
+              </Card>
+            </Tab>
+            <Tab key="template" title="Template">
+              <Card className="p-0" shadow="none">
+                <CardBody className="p-4">
+                  <div className="mb-6 flex items-center gap-[5px] mb-3 md:mb-4 ">
+                    <LayoutTemplate className="w-5 h-5 text-xl text-secondary-400" />
+                    <h1 className=" text-xl/[24px] font-semibold text-tertiary  md:text-[20px]/[24px]">Template</h1>
+                  </div>
+                  <div className="flex gap-4">
+                    <div className="w-4/5">
+                      <div className="grid grid-cols-1 gap-4">
+                        <Textarea variant="bordered" size="sm" rows={100} label="Invitation Template" name="invitationTemplate" value={values.invitationTemplate || ''} onChange={handleChange} isInvalid={!!errors.invitationTemplate && !!touched.invitationTemplate} errorMessage={errors.invitationTemplate} />
+                      </div>
+                    </div>
+                    <div className="w-1/5">
+                      <div className="lg:col-span-1">
+                        <Card>
+                          <CardHeader>Company Logo</CardHeader>
+                          <CardBody>
+                            <div className="space-y-4">
+                              <div className="flex items-center justify-center w-full h-32 border-2 border-dashed border-secondary-200 rounded-lg bg-secondary-50">
+                                <div className="text-center">
+                                  {uploadLogoUrl && (
+                                    <div className="relative inline-block">
+                                      <img src={uploadLogoUrl} alt="Company Logo Preview" className="max-w-full h-auto rounded max-w-[80px]" />
+                                      <button type="button" onClick={handleRemoveImage} className="absolute -top-3 -right-3">
+                                        <AiOutlineCloseCircle />
+                                      </button>
+                                    </div>
+                                  )}
+                                  <p className="text-sm text-gray-500">Current Logo</p>
+                                </div>
+                              </div>
+
+                              <FileUploadWithPreview
+                                onUpload={(data) => {
+                                  setFieldValue('logo', data.url);
+                                  setUploadLogoUrl(data.url);
+                                }}
+                              />
+                              <p className="text-xs text-gray-500 text-center">Supports: JPG, PNG, SVG (Max 2MB)</p>
+                            </div>
+                          </CardBody>
+                        </Card>
+                      </div>
+                    </div>
+                  </div>
+                </CardBody>
+              </Card>
+            </Tab>
+          </Tabs>
+          <div>
+            <Button type="submit" startContent={<Save />} isLoading={isLoading} color="secondary" variant="flat" radius="full">
+              Save Settings
+            </Button>
+          </div>
         </Form>
       )}
     </Formik>
