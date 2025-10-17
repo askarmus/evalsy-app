7dafcc07a6159fce2e6b9c2245ee1f20ca809138
askarmus <askarmus@hotmail.com>
2025-10-17 23:20:39 +0530

    .

--- app/Supabase/page.tsx
+++ app/Supabase/page.tsx
@@ -3,12 +3,12 @@
 'use client';
 
 import HomeLayout from '../home-layout';
-import { Header } from '@/components/start/Header';
 import { Footer } from '@/components/start/Footer';
 import CookieBanner from '@/components/start/CookieBanner';
 import { useEffect, useState } from 'react';
 import ResumeDropzone from './components/ResumeDropzone';
 import ResumeList from './components/ResumeList';
+import { Header } from '@/components/navbar/navbar';
 
 export default function TermsPage() {
   const sections = ['Definitions', 'Eligibility', 'Services Provided', 'User Responsibilities', 'Accounts and Security', 'Intellectual Property', 'Data Protection & Privacy', 'Payment Terms', 'Limitation of Liability', 'Termination', 'Governing Law', 'Changes to Terms', 'Contact Us'];
@@ -42,10 +42,8 @@ export default function TermsPage() {
         <main className="flex-1">
           <Header />
           <div className="max-w-7xl mx-auto px-6 py-12 grid grid-cols-1 lg:grid-cols-4 gap-10">
-       
-
-    <ResumeDropzone />
-      <ResumeList />
+            <ResumeDropzone />
+            <ResumeList />
           </div>
           <CookieBanner />
         </main>
